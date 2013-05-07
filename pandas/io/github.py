b4ae6bb5457b68ff46e41ddb64785b2b9eb24be6
Thomas Levine <occurrence@thomaslevine.com>
2013-05-07 17:59:30 -0400

    hacky start at a github issue importer

---
+++ pandas/io/github.py
@@ -0,0 +1,36 @@
+import json
+
+import requests
+
+from pandas import DataFrame
+
+def _flatten_issue(issue):
+    '''issue : dict
+    One issue from the list of issues, containing such things as a body and labels
+    '''
+    return {
+        u'assignee': issue[u'assignee'],
+        u'body': issue[u'body'],
+        u'closed_at': issue[u'closed_at'],
+        u'created_at': issue[u'created_at'],
+        u'comments': issue[u'comments'],
+        u'id': issue[u'id'],
+#       u'labels',
+#       u'milestone',
+        u'number': issue[u'number'],
+#       u'pull_request',
+        u'state': issue[u'state'],
+        u'title': issue[u'title'],
+        u'updated_at': issue[u'updated_at'],
+        u'url': issue[u'url'],
+        u'user': issue[u'user'][u'html_url'].split('/')[-1],
+    }
+
+
+
+def get_issues(owner, repo):
+#              label = None, not_label = None,
+#              has_milestone = None, not_milestone = None):
+    r = requests.get('https://api.github.com/repos/%s/%s/issues' % (owner, repo))
+    issues = json.loads(r.text)
+    return DataFrame([_flatten_issue(issue) for issue in issues])
